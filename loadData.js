66a898fbf912612c82643fa44eedf6628ba70f34
fern1994 <hemhongsa.1994@gmail.com>
2018-05-15 20:50:17 +0700

    add userId

---
+++ loadData.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  // user.html?id=27
+  var url = new URL(window.location.href);
+  var x = url.searchParams.get("id")
+  $.ajax({
+      url: 'http://188.166.237.168/api/user/'+x,
+      dataType: 'json',
+      type: 'GET',
+      success: function (data) {
+        console.log(data);
+        $('#prefix').val(data[0].prefix);
+        $('#first_name').val(data[0].first_name);
+        $('#last_name').val(data[0].last_name);
+        $('#email').val(data[0].email);
+        $('#username').val(data[0].username);
+      }
+  });
+});
